cff8babf572a6bdbd0148ccfba8680afccd61eda
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2025-02-04 10:58:19 +0530

    configured tsconfig.json

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -3,7 +3,11 @@ type GreetingsProps = {
 };
 
 const Greetings = ({ name }: GreetingsProps) => {
-  return <>{`Greetings, ${name}!`}</>;
+  return (
+    <div className="text-xl font-semibold text-blue-600 bg-gray-100 p-4 rounded-lg shadow-md">
+      {`Greetings, ${name}!`}
+    </div>
+  );
 };
 
 export default Greetings;
